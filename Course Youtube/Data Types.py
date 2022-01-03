9cc9b6e996629aac38815d90b3ee578ab99c27ed
Franco Tejo <francotejo@gmail.com>
2022-01-02 22:03:03 -0300

    se agrego un archivo con tipos de datos

---
+++ Course Youtube/Data Types.py
@@ -0,0 +1,15 @@
+tiposDatosVariables = -1
+
+print(tiposDatosVariables, type(tiposDatosVariables))
+
+tiposDatosVariables = True
+
+print(tiposDatosVariables, type(tiposDatosVariables))
+
+tiposDatosVariables = 3.141516
+
+print(tiposDatosVariables, type(tiposDatosVariables))
+
+tiposDatosVariables = "abcde"
+
+print(tiposDatosVariables, type(tiposDatosVariables))
