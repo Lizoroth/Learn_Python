b49226974641b2b0e7adcb393f1a01681f42b232
Franco Tejo <francotejo@gmail.com>
2022-01-05 20:12:06 -0300

    Upgrade a python 3.10, uso de case y funciones

--- Course Youtube/Exercises/Functions.py
+++ Course Youtube/Exercises/Functions.py
@@ -1,15 +1,50 @@
-def sumaDosNumeros(a,b):
-    c = a+b
-    return c
+import os
+from typing import Match
 
-#print (sumaDosNumeros(5,20))
+aux = 1
+
+def sumaNumeros(x,z):
+  return x+z
+
+def restaNumeros(x,z):
+  return x-z
+
+def divideNumeros(x,z):
+  return x/z
+
+def multiplicaNumeros(x,z):
+  return x*z
+
+def clearConsole():
+    command = 'clear'
+    if os.name in ('nt', 'dos'):  # If Machine is running on Windows, use cls
+        command = 'cls'
+    os.system(command)
+
+def inNumeros(aux):
+  if (aux != 0):
+    x = int(input("Ingrese primer núnmero"))
+    z = int(input("Ingrese segundo número"))
+    match int(aux):
+      case 1: print(sumaNumeros(x,z))
+      case 2: print(restaNumeros(x,z)) 
+      case 3: print(divideNumeros(x,z))
+      case 4: print(multiplicaNumeros(x,z))    
+
+while aux != 0:
+  
+  clearConsole()
+  print("Bienvenido al menú de operaciones")
+  print("seleccione una opción")
+  print("1 - Suma")
+  print("2 - Resta")
+  print("3 - División")
+  print("4 - Multiplicación")
+  print("0 - Salir")
+  aux = int(input("Ingrese Opción"))
+  inNumeros(aux)
 
 
-def numerosPrimos(nro):    
-    for x in range(nro,0,):# range(start,stop,step)
-        if nro%x == 1 :
-          print(x)
-        else :    print("no")
-    
 
-numerosPrimos(2)
+
+
